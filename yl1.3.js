38acb3bb1d999f5716791a405d720f135c4c8ffb
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-09-26 14:09:18 +0300

    Ylesanne 1.3.2

--- yl1.3.js
+++ yl1.3.js
@@ -1,4 +1,11 @@
-let astmeAlus=2
-let astendaja=4
-let tulemus=astmeAlus**astendaja
-console.log(tulemus)
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question(`Sisestage astme alus: `, astmeAlus=>{
+    let astendaja =4
+    let tulemus=astmeAlus**astendaja
+    console.log(tulemus)
+    rl.close()
+})
