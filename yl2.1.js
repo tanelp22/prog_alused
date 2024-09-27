0ac8f0d3f9883f8a1b7657e7321ba68748362350
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-09-27 09:12:00 +0300

    Ylesanne 2.1 erienvad võimalused

--- yl2.1.js
+++ yl2.1.js
@@ -4,9 +4,14 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 rl.question(`Sisesta õhutemperatuur: `, temperatuur=>{
-    if (temperatuur<4.0){
-        console.log("On jäätumise oht.")
-    }
-    else{console.log("Ei ole jäätumise ohtu.")} 
+    //if (temperatuur<4.0){
+    //    console.log("On jäätumise oht.")
+    //}
+    //else{console.log("Ei ole jäätumise ohtu.")} 
+    
+    //vastus=(temperatuur<4.0) ? "On jäätumise oht." : "Ei ole jäätumise ohtu." ;
+    //console.log(vastus)
+    
+    console.log((temperatuur<4.0) ? "On jäätumise oht." : "Ei ole jäätumise ohtu." )
     rl.close()
 })
